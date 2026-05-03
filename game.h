dd4bd91a60a986ccf0871c0658fd0d3c74db9d2e
oliver.donnelly <oliverhdonnelly@icloud.com>
2026-05-03 18:13:54 +1000

    feat. function declerations and struct
    chore:
    - def functions starting with print menu and add game

---
+++ game.h
@@ -0,0 +1,27 @@
+#ifndef GAME_H
+#define GAME_H
+#include <iostream>
+
+struct Game
+{
+    int id;
+    std::string title;
+    std::string description;
+    int rating;
+};
+
+void printMenu();
+
+void addGame(std::vector<Game>& games, int& nextId);
+
+void viewGames(std::vector<Game>& games);
+
+void searchGames(std::vector<Game>& games);
+
+void sortGames(std::vector<Game>& games);
+
+void saveToFile(const Game& game);
+
+void loadFromFile(std::vector<Game>& games, int& nextId);
+
+#endif
